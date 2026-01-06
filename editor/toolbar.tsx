47c10051c96f3f4389c32d5552b7e3cb6dda0c20
JakeSiewJK64 <joekanesiew@outlook.com>
2026-01-06 16:16:44 +0800

    toolbar - add title

--- editor/toolbar.tsx
+++ editor/toolbar.tsx
@@ -13,18 +13,21 @@ export function Toolbar({ editor }: ToolbarProps) {
   return (
     <div className="mb-2 flex flex-row rounded border border-slate-300 p-1">
       <button
+        title="Bold"
         className="cursor-pointer p-1 hover:bg-slate-300"
         onClick={() => CustomEditorHelper.toggleBoldMark(editor)}
       >
         <LuBold />
       </button>
       <button
+        title="Undo"
         className="cursor-pointer p-1 hover:bg-slate-300"
         onClick={() => editor.undo()}
       >
         <LuUndo />
       </button>
       <button
+        title="Redo"
         className="cursor-pointer p-1 hover:bg-slate-300"
         onClick={() => editor.redo()}
       >
